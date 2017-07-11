7bc5df8a12801e3f5cc5310f68d55bdd734bfbd4
dbanuti <dbanuti@stanford.edu>
2017-07-11 14:25:06 -0700

    Added comments to GAmath.py

--- GAmath.py
+++ GAmath.py
@@ -5,51 +5,59 @@ from random import *
 
 
 def minimum(list):
-        return min(list)
+    return min(list)
 
 def maximum(list):
-        return max(list)
+    return max(list)
 
 def fitsum(list):
-        sum=0.0
-        for i in range(0,len(list)):
-            sum+=list[i]
-        return sum
-        
+    """Sum of fitnesses in the list, needed to build 
+    wheel of fortune."""
+    sum=0.0
+    for i in range(0,len(list)):
+        sum+=list[i]
+    return sum
+    
 def avg(list):
-        sum=fitsum(list)
-        return (sum/len(list))
+    """Returns average fitness of entity in list."""
+    sum=fitsum(list)
+    return (sum/len(list))
             
 def bin_dec(bin):
-        dec=0.0
-        bin.reverse()
-        for i in range(0, len(bin)):
-            dec+=(bin[i]*(2**i))
-        return dec
+    """Conversion binary -> decimal. Needed to calculate 
+    decimal variable value from binary coded genome."""
+    dec=0.0
+    bin.reverse()
+    for i in range(0, len(bin)):
+        dec+=(bin[i]*(2**i))
+    return dec
             
 
 def dec_bin(dec):
-        #
-        pass
+    #
+    pass
 
 
 def flip():
-        tmp=decide(0.50)
-        return tmp
+    """Flips a coin. Returns 1 or 0 with 0.5 probability, respectively."""    
+    tmp=decide(0.50)
+    return tmp
            
 def decide(prob):
-        tmp=random()
-        if tmp>=prob:
-            return 0
-        if tmp<prob:
-            return 1
+    """ Returns true with 'prob' probability as a way to 
+    decide the outcome of an event."""
+    tmp=random()
+    if tmp>=prob:
+        return 0
+    if tmp<prob:
+        return 1
 
 def rangeint(lo, hi):
-        if hi>lo:
-            tmp=randrange(lo, hi)
-            return tmp
-        else:
-            return hi
+    if hi>lo:
+        tmp=randrange(lo, hi)
+        return tmp
+    else:
+        return hi
 
 def rangefloat(lo, hi):
         pass
